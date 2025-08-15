20ace68a4f08167c84a4446b823db8dbba745e69
Luis Morua <lfdomc@gmail.com>
2025-08-15 15:43:53 -0600

    1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -68,7 +68,7 @@ export default function Footer() {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="h-5 w-5 text-blue-400" />
-                <span className="text-gray-300">info@hgremodelaciones.com</span>
+                <span className="text-gray-300">fernando.apuy@qualitascr.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="h-5 w-5 text-blue-400" />
